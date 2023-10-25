2f980239bb374eb7404323c9636122fe35680463
Wagner Herculano <wagherculano@hotmail.com>
2023-10-25 15:59:20 -0300

    Update database.py
    Configuracao MySQL adicionada novamente

--- database.py
+++ database.py
@@ -2,7 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./escola.db"
+# Substitua as informações abaixo pelas configurações do seu servidor MySQL
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "nova_senha"
+MYSQL_HOST = "172.17.0.2"
+MYSQL_DB = "projeto_escola"
+
+# Crie a URL de conexão do SQLAlchemy para o MySQL com o driver mysql-connector-python
+DATABASE_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
