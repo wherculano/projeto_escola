f49e1ce8d696aa6d3851eb8dcb67aa07f97ca2e2
Wagner Herculano <wagherculano@hotmail.com>
2023-10-24 21:42:50 -0300

    Update database.py
    Change from MySQL to SQLite

--- database.py
+++ database.py
@@ -1,18 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-# Substitua as informações abaixo pelas configurações do seu servidor MySQL
-MYSQL_USER = os.environ['MYSQL_USER']
-MYSQL_PASSWORD = os.environ['MYSQL_PASSWORD']
-MYSQL_HOST = os.environ['MYSQL_HOST']
-MYSQL_DB = os.environ['MYSQL_DB']
-
-# Crie a URL de conexão do SQLAlchemy para o MySQL com o driver mysql-connector-python
-DATABASE_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+DATABASE_URL = "sqlite:///./escola.db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
